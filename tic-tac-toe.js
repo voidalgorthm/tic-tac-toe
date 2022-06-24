ee0f6e9f1789c75d108467589c894a96e0c12569
voidalgorthm <3atlas.navis.noos@gmail.com>
2022-06-24 23:05:56 +0800

    feat: add gameboard interactions of the game

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -29,3 +29,28 @@ const switchFaction = (sign) => {
 
 const _humanPlayer = Player('X');
 const _aiPlayer = Player('O');
+
+const gameBoard = (() => {
+    let _board = [...Array(9)];
+    const getTiles = (num) => _board[num];
+
+    const setTiles = (num, player) => {
+        const gameTiles = document.querySelectorAll(`button.btn-game`);
+        const gameTile = gameTiles.item(`${num + 1})`);
+        gameTile.classList.add('marked');
+        gameTile.textContent = player.getSign();
+        _board[num] = player.getSign();
+        gameTile.setAttribute('disabled', true);
+    }
+
+    const clearTiles = () => {
+        _board = _board.map(tile => tile = undefined);
+    }
+    return {
+        getTiles,
+        setTiles,
+        clearTiles
+    };
+})();
+
+const setAttributes = (elem, attrs) => { Object.entries(attrs).forEach(([key, value]) => elem.setAttribute(key, value)); }
